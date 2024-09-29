bc73d28e3a0c9fb92f9a4ec63bb12d0cf04effec
Jiwon <ysjjw2003@naver.com>
2024-09-29 13:46:25 +0900

    ADD img

--- emotion_diary/src/App.jsx
+++ emotion_diary/src/App.jsx
@@ -1,19 +1,54 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Notfound from "./pages/Notfound";
 
+// 이미지 불러오기
+import emotion1 from "./assets/emotion1.png";
+import emotion2 from "./assets/emotion2.png";
+import emotion3 from "./assets/emotion3.png";
+import emotion4 from "./assets/emotion4.png";
+import emotion5 from "./assets/emotion5.png";
+
+// 페이지 라우팅 설정
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("./new"); // 페이지 이동
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-    </Routes>
+    // Routes 컴포넌트 안에는 Route 컴포넌트만 들어갈 수 있다.
+    // Link 컴포넌트는 a 태그와 다르게 새로고침 하지 않고 페이지를 변환한다.
+    // :id은 url 파라미터를 나타냄
+    <>
+      <div>
+        <img src={emotion1} />
+        <img src={emotion2} />
+        <img src={emotion3} />
+        <img src={emotion4} />
+        <img src={emotion5} />
+      </div>
+
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
   );
 }
 
--- emotion_diary/src/pages/Diary.jsx
+++ emotion_diary/src/pages/Diary.jsx
@@ -1,5 +1,10 @@
+import { useParams } from "react-router-dom"; // url 파라미터의 값을 가져옴
+
 const Diary = () => {
-  return <div>Diary</div>;
+  const params = useParams();
+  console.log(params);
+
+  return <div>{params.id}번 일기입니다~</div>;
 };
 
 export default Diary;
