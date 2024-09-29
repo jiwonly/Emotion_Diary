8c9e027cb42c0631ee4f46af97a4689a54decef7
Jiwon <ysjjw2003@naver.com>
2024-09-29 12:47:21 +0900

    ADD route

---
+++ emotion_diary/src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom"; // 브라우저 현재 주소 저장하고 감지
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
